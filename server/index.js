a609e5e7900937573c4226381b35e3087e62c33a
Vinothini31 <vinothinim012@gmail.com>
2025-10-24 22:38:45 +0530

    Prepare MERN app for Render deployment

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/contact', contactRoutes); // ✅ Use route here
 
 // Start server
+const PORT = process.env.PORT || 5000; 
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
 });
